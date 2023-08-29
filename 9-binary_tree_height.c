f50671c96bd8a8f3cd6673bd9800fd66843751c7
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-08-29 12:52:38 +0100

    Task 9 fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t left = 0, right = 0;
+		size_t l = 0, r = 0;
 
-		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((left > right) ? left : right);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
 	}
 	return (0);
 }
