7e6a8a865c7127a25fb10bf89ad780efb93d9aaa
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-08-29 11:18:20 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -13,14 +13,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
