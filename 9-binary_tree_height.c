e6e25abf6824e2aeefcab4e60e72543d809f1f34
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-08-29 12:55:52 +0100

    Task 9 udate

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,10 +6,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t l, r;
+
+	r = 0;
+	l = 0;
+
 	if (tree)
 	{
-		size_t l = 0, r = 0;
-
 		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((l > r) ? l : r);
