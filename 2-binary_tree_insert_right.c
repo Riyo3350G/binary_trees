8ec2b29ba282b218f8c8b38fe0f5015d6775e988
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-08-29 12:08:53 +0100

    fix: task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,13 +8,12 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *right_node;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *right_node;
-
 	right_node = binary_tree_node(parent, value);
-
 	if (right_node == NULL)
 		return (NULL);
 
