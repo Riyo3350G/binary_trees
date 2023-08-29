40df9880079b962ff400d4a9d1632f964e8039c3
riyo3350g <yechouben@gamail.com>
2023-08-29 12:50:13 +0100

    Task 9 fixing

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,15 +6,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+	if (tree)
+	{
+		size_t left = 0, right = 0;
 
-	left = right = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return ((left > right) ? left : right);
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left > right) ? left : right);
+	}
+	return (0);
 }
