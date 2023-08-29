5b4fa6242b72c2b05036446191aa240e9727efe1
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-08-29 11:48:15 +0100

    Task 0 var name change

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,15 +9,15 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node;
+        binary_tree_t *node;
 
-        new_node = malloc(sizeof(binary_tree_t));
-        if (new_node == NULL)
+        node = malloc(sizeof(binary_tree_t));
+        if (node == NULL)
 		return (NULL);
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
 
-	return (new_node);
+	return (node);
 }
